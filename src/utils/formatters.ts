d177feaf2bbc764bcbc35e9d5ab87e5cb9a5b086
TMS126 <94375437+TMS126@users.noreply.github.com>
2026-07-17 09:55:16 +0200

    Delete src directory

--- src/utils/formatters.ts
+++
@@ -1,7 +0,0 @@
-export const formatZAR = (amount: number): string => {
-  return new Intl.NumberFormat('en-ZA', {
-    style: 'currency',
-    currency: 'ZAR',
-    minimumFractionDigits: 2,
-  }).format(amount || 0);
-};
